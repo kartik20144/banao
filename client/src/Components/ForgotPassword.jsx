dc2020929e4c1c9405f23b869d916dbe5325ea46
Kartik <kartikagarwal20144@gmail.com>
2024-05-03 02:03:47 +0530

    first commit

---
+++ client/src/Components/ForgotPassword.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "../App.css";
+import { useState } from "react";
+import Axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+
+const ForgotPassword = () => {
+
+    const [email, setEmail] = useState("");
+  
+    const navigate = useNavigate();
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      Axios.post("http://localhost:3000/auth/forgot-password", {
+        email,
+      })
+        .then((response) => {
+          if (response.data.status) {
+            alert("Check your mail for reset password link");
+            navigate("/login");
+          }
+        
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+  return (
+    <div className="sign-up-container">
+    <form className="sign-up-form" onSubmit={handleSubmit}>
+      <h2>Forgot Password</h2>
+
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        autoComplete="off"
+        placeholder="Email"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+
+      <button type="submit">Send</button>
+    </form>
+  </div>
+  )
+}
+
+export default ForgotPassword
---
+++ client/src/Components/Login.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "../App.css";
+import { useState } from "react";
+import Axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  Axios.defaults.withCredentials = true;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    Axios.post("http://localhost:3000/auth/login", {
+      username,
+      password,
+    })
+      .then((response) => {
+        if (response.data.status) {
+          navigate("/");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="sign-up-container">
+      <form className="sign-up-form" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          placeholder="******"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button type="submit">Login</button>
+        <Link to="/forgotPassword">Forgot Password? </Link>
+        <p>
+          Don't have an account? <Link to="/signup">Sign Up</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
